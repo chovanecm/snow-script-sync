66297b293d9d24aa198160e6d4a880a6ecb9d079
Martin Chovanec <chovamar@fit.cvut.cz>
2015-04-06 20:41:21 +0200

    escape \\n

--- src/test/java/cz/chovanecm/snow/TestSingleGet.java
+++ src/test/java/cz/chovanecm/snow/TestSingleGet.java
@@ -8,6 +8,8 @@ package cz.chovanecm.snow;
 import cz.chovanecm.snow.records.SnowScript;
 import cz.chovanecm.snow.tables.ScriptSnowTable;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.Test;
 
 /**
@@ -27,6 +29,6 @@ public class TestSingleGet {
         SnowClient client = new SnowClient("https://demo019.service-now.com", "admin", "admin", null, null);
         SnowScript script = client.getRecordBySysId(new ScriptSnowTable("sys_script_include", "script", "name"), "3cdeec09a997b940234722fdcbb24210", SnowScript.class);
         System.out.println(script.getScript());
-        
+        Files.write(Paths.get("D:/test.js"), script.getScript().getBytes());
     }
 }
